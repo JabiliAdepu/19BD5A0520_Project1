67df70d2b4d53e705d767cb086adec5d92fedc62
Jabili Adepu <jabili.adepu@gmail.com>
2020-10-03 14:05:49 +0530

    JWT Authenticated CRUD App

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mDB = require("mongoose");
 const url = "mongodb://localhost:27017/hospitalInventory";
+const middleware = require("./middleware"); 
 
 mDB.set('useNewUrlParser', true);
 mDB.set('useFindAndModify', false);
@@ -16,10 +17,10 @@ con.then((db) => {
 });
 
 const hospRouter = require("./routes/hospitalData");
-app.use("/hospitalData", hospRouter);
+app.use("/hospitalData", middleware.checkToken, hospRouter);
 
 const ventilatorRouter = require("./routes/ventilatorData");
-app.use("/ventilatorData", ventilatorRouter);
+app.use("/ventilatorData", middleware.checkToken, ventilatorRouter);
 
 app.listen(3000, function(){
     console.log("Server started");
